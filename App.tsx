e861b9736986738ca8d94af29438b33a1d123338
Gustavo <guh-51@hotmail.com>
2024-01-31 09:35:55 -0300

    refact: app.tsx

--- App.tsx
+++ App.tsx
@@ -10,11 +10,12 @@ import { Login } from "@screens/Login";
 import { Register } from "@screens/Register";
 import { Routes } from "./src/routes";
 import { ProductDetails } from "@screens/ProductDetails";
+import { Home } from "@screens/Home";
 export default function App() {
   const [fontsLoaded] = useFonts({Karla_400Regular, Karla_700Bold})
   return (
     <NativeBaseProvider theme={THEME}>
-        {fontsLoaded ? <ProductDetails/> : []}
+        {fontsLoaded ? <Home/> : []}
         <StatusBar style="auto" />
     </NativeBaseProvider>
   );
